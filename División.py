2924ea135fb10d7b7a31bd3aef59f17f72dbc1d6
ralucaroiban <aroiban20@iesrioarba.es>
2024-10-08 09:52:06 +0200

    Add files via upload

---
+++ División.py
@@ -0,0 +1,14 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def C(a, b):
+    pass
+
+def R(a, b):
+    pass
+
+a = int(input('Enter D: '))
+b = int(input('Enter d: '))
+
+print(f'COCIENTE de {a} and {b} es {C(a, b)}')
+print(f'RESTO de  {a} and {b} es {R(a, b)}')
